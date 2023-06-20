b50a040cc5307741959b7e6819a473cb88e04f26
devcorp tech <mohamedboulhaj92@gmail.com>
2023-06-20 14:44:42 +0000

    --

--- ecom-app/src/main/java/com/yassa/ecomapp/securite/KeycloakConfig.java
+++ ecom-app/src/main/java/com/yassa/ecomapp/securite/KeycloakConfig.java
@@ -10,6 +10,8 @@ import org.keycloak.admin.client.resource.RealmResource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 
 @Configuration
